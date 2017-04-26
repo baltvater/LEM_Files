555000c9705291c0f6f5eed6195d24282b951f9a
baltvater <bill.altvater@healthstream.com>
2017-04-26 11:03:03 -0500

    card 34 files

---
+++ LEM_Files/Mingle_34/HSTM.Platform.Web/Scripts/EventManagement/eventsettings.js
@@ -0,0 +1,33 @@
+﻿    var mHasUserClickedCrossOrgCheckbox = false;
+    var mRegistrationAccessOptions;
+
+    function removeAdminOnlyOption() {
+        $("#RegistrationAccessOptionId option[value=1]").remove();
+    }
+
+    function changeSelfRegistrationAndAdminText(text) {
+        $("#RegistrationAccessOptionId option[value=2]").text(text);
+    }
+
+            mRegistrationAccessOptions = $("#RegistrationAccessOptionId option").clone();
+
+            if($("#IsGlobal").is(":checked")) {
+                removeAdminOnlyOption();
+                changeSelfRegistrationAndAdminText(uiResources.SelfRegistrationAndAdminOfHostingOrganization);
+            }
+
+            $("#IsGlobal").click(function () {
+                if(!mHasUserClickedCrossOrgCheckbox && $(this).is(":checked")) {
+                    $("#lnkOpenCrossOrganizationHelpModal").click();
+                    mHasUserClickedCrossOrgCheckbox = true;
+                }
+
+                if($(this).is(":checked")) {
+                    removeAdminOnlyOption();
+                    changeSelfRegistrationAndAdminText(uiResources.SelfRegistrationAndAdminOfHostingOrganization);
+                }
+                else {
+                    $("#RegistrationAccessOptionId").html(mRegistrationAccessOptions);
+                    changeSelfRegistrationAndAdminText(uiResources.SelfRegistrationAndAdmin);
+                }
+            });
